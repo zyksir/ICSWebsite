447eda71f7dad34129c814943ecdf56d22965253
mayqinxu <675341471@qq.com>
2018-12-13 10:52:31 +0800

    前端设置更新
    都对接上了，但是修改密码需要后端再写一下函数

--- flaskr/flaskr/user.py
+++ flaskr/flaskr/user.py
@@ -32,15 +32,37 @@ def home(id):
     return render_template('user/temp_home.html', user=user)
 
 
-@bp.route('/set', methods=('GET', 'POST'))
-def set():
+@bp.route('/setname', methods=('GET', 'POST'))
+def setname():
     if request.method == 'POST':
         nickname = request.form['nickname']
-        email = request.form['email']
         error = None
 
         if not nickname:
             error = 'Nickname is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            conn, db = get_db()
+            db.execute(
+                'UPDATE user SET nickname = %s'
+                ' WHERE id = %s',
+                (nickname, g.user['id'])
+            )
+            conn.commit()
+
+            return redirect(url_for('blog.index'))
+
+    return render_template('user/temp_setnickname.html')
+
+
+@bp.route('/setemail', methods=('GET', 'POST'))
+def setemail():
+    if request.method == 'POST':
+        email = request.form['email']
+        error = None
+
         if not email:
             error = 'Email is required.'
 
@@ -49,12 +71,50 @@ def set():
         else:
             conn, db = get_db()
             db.execute(
-                'UPDATE user SET nickname = %s, email = %s'
+                'UPDATE user SET email = %s'
                 ' WHERE id = %s',
-                (nickname, email, g.user['id'])
+                (email, g.user['id'])
             )
             conn.commit()
 
             return redirect(url_for('blog.index'))
 
-    return render_template('user/easy_set.html')
+    return render_template('user/temp_setemail.html')
+
+
+@bp.route('/setpass', methods=('GET', 'POST'))
+def setpass():
+    if request.method == 'POST':
+        '''
+        nowpass 原来的密码
+        password 新的密码
+        repassword 重新输入新密码
+        if not后的东西要重写
+        以及error可以中文，应该是可以输出到前端的如果我没写错的话
+        '''
+        nowpass = request.form['nowpass']
+        password = request.form['password']
+        repassword = request.form['repassword']
+        error = None
+
+        if not password:
+            error = 'Password is required.'
+        elif not (password ==repassword):
+            error = 'Two passwords are inconsistent.'
+        elif not (password == repassword):
+            error = 'you enter different passwords!'
+
+        if error is not None:
+            flash(error)
+        else:
+            conn, db = get_db()
+            db.execute(
+                'UPDATE user SET password = %s'
+                ' WHERE id = %s',
+                (password, g.user['id'])
+            )
+            conn.commit()
+
+            return redirect(url_for('blog.index'))
+
+    return render_template('user/temp_setpass.html')
